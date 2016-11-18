63a6bf4791479709c145bb5897945defbac9ecfb
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-11-18 12:11:39 +0100

    Inizializzazione applicazione

---
+++ clienti.php
@@ -0,0 +1,18 @@
+<?php
+$title = 'Pagina creazione cliente';
+?>
+
+<!-- header -->
+<?php
+include './templates/header.php';
+?>
+
+<!-- contenuto pagina -->
+<table>
+
+</table>
+
+<!-- footer -->
+<?php
+include './templates/footer.php';
+?>
