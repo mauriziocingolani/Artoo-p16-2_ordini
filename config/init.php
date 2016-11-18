e4eb7cee393b7b87460aa86b361e377d7d1a6853
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-11-18 14:46:23 +0100

    File di bootstrap

--- config/init.php
+++ config/init.php
@@ -1,6 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once './classi/Database.php';
 include_once './classi/Cliente.php';
 
-
-
+$db = Database::GetIstanza('localhost', 'root', 'antani1234');
+$db->chiedi('USE artoo');
